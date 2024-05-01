07bd848d0c0953148df20f07ec5785b9a0f94562
Jelte Dirks <j.dirks@outlook.com>
2024-05-01 22:08:31 +0200

    some crude parsing
    Should solve some crude parsing that works for a few test cases so I can
    continue with the rest of the program logic.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,4 +1,6 @@
-use std::fmt::Display;
+use std::{fmt::Display, io::BufRead};
+
+use tokio::io::AsyncBufReadExt;
 
 /* Request Line grammar can be found here:
  * https://httpwg.org/specs/rfc9112.html#message.format
@@ -36,6 +38,7 @@ pub struct HttpRequest {
     cursor: usize,
     method: Option<HttpMethod>,
     target: Option<String>,
+    version: Option<HttpVersion>,
 }
 
 impl HttpRequest {
@@ -44,7 +47,10 @@ impl HttpRequest {
     pub fn add_bytes(&mut self, bytes: &[u8]) -> Result<bool, HttpError> {
         self.bytes.extend_from_slice(&bytes);
 
-        // When method is not found yet, it should be explored
+        // TODO: parsing could be done more efficiently.
+        // e.g.: iterateover the bytes and find the spaces, when spaces are
+        // found, do something with the parts in between.
+
         if self.method.is_none() {
             if self.bytes.len() > 1 {
                 let method = match self.bytes[0] {
@@ -73,7 +79,6 @@ impl HttpRequest {
         }
 
         if self.method.is_some() && self.target.is_none() {
-            // TODO: needs some edge cases with better error handling
             let next_sp = self.bytes[self.cursor..]
                 .iter()
                 .position(|&byte| byte == 0x20);
@@ -92,6 +97,26 @@ impl HttpRequest {
             }
         }
 
+        if self.target.is_some() && self.version.is_none() {
+            let lines = self
+                .bytes
+                .split(|&byte| byte == 0x0A)
+                .take(1)
+                .flat_map(|first_line| first_line.split(|&byte| byte == 0x20).skip(2).take(1))
+                .map(|version| version)
+                .collect::<Vec<_>>();
+
+            if lines.len() == 0 {
+                return Err(HttpError::BadFormat);
+            }
+
+            let version: HttpVersion = lines[0].try_into()?;
+
+            self.version = Some(version);
+        }
+
+        dbg!(&self);
+
         Ok(false)
     }
 }
@@ -103,6 +128,7 @@ impl Default for HttpRequest {
             cursor: Default::default(),
             method: Default::default(),
             target: Default::default(),
+            version: Default::default(),
         }
     }
 }
@@ -113,6 +139,26 @@ impl Display for HttpError {
         match self {
             HttpError::UnknownVersion => write!(f, "Http version seems to be unknown"),
             HttpError::BadFormat => write!(f, "Http seems to be wrongly formatted"),
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for HttpVersion {
+    type Error = HttpError;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        if value.len() < 8 {
+            return Err(HttpError::BadFormat);
+        }
+
+        match value[0..=4] {
+            [0x48, 0x54, 0x54, 0x50, 0x2F] => match value[5..=7] {
+                [0x31, 0x2E, 0x30] => Ok(HttpVersion::Http10),
+                [0x31, 0x2E, 0x31] => Ok(HttpVersion::Http11),
+                [0x32, _] => Ok(HttpVersion::Http2),
+                [0x33, _] => Ok(HttpVersion::Http3),
+                _ => Err(HttpError::UnknownVersion),
+            },
+            _ => Err(HttpError::BadFormat),
         }
     }
 }
