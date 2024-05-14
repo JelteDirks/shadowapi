3768295310402ad5c63559bd6367edac83311d07
Jelte Dirks <j.dirks@outlook.com>
2024-05-14 22:15:21 +0200

    some testing functionality in express
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- testservers/shadow.js
+++ testservers/shadow.js
@@ -10,8 +10,29 @@ app.get('/api', async (req, res) => {
     process.stderr.write(JSON.stringify(req.headers) + "\n");
   }
   const delay = Math.random();
-  await sleep(10 * delay);
+  await sleep(1000 * delay);
   res.json({ "content": "some content" });
+});
+
+app.post('/file', async (req, res) => {
+  if (req.headers) {
+    process.stderr.write(JSON.stringify(req.headers) + "\n");
+  }
+  res.send(Buffer.from('This is some binary data', 'utf-8'));
+});
+
+app.post('/html', async (req, res) => {
+  if (req.headers) {
+    process.stderr.write(JSON.stringify(req.headers) + "\n");
+  }
+  res.send('<html><body><h1>Hello, World!</h1></body></html>');
+});
+
+app.post('/json', async (req, res) => {
+  if (req.headers) {
+    process.stderr.write(JSON.stringify(req.headers) + "\n");
+  }
+  res.json({ name: 'John Doe', email: 'john.doe@example.com' });
 });
 
 app.listen(port, () => {
