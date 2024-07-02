2ba007a7044c635da25cbc16ca35caf1f29ac9b6
Jelte Dirks <j.dirks@outlook.com>
2024-07-02 16:34:31 +0200

    fix offset check and add more headers
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/http/response.rs
+++ src/http/response.rs
@@ -64,7 +64,7 @@ pub fn build_header_pair(buf: &[u8], l: usize, r: usize, h: HttpHeader) -> Optio
 }
 
 pub fn len_match(buf: &[u8], s: usize, e: usize, offset: usize) -> bool {
-    return buf[s+offset] == b':' && e < s+offset;
+    return buf[s+offset] == b':' && s+offset < e;
 }
 
 // buf: ref to the raw bytes
@@ -74,30 +74,23 @@ pub fn decode_header(buf: &[u8], s: usize, e: usize) -> Option<HttpHeaderPair> {
     // NOTE: end is including the position of \n, in http there are CRLF line
     // endings which mean there is a \r before \n at position [end - 1].
 
-    println!("{:?}", build_header_pair(buf, s, e, HttpHeader::Tk));
-
     match buf[s] {
         b'A' => { /* A */
             match buf[s + 1] {
-                b'g' => { /* Ag */
-                    if len_match(buf, s, e, 3) {
-                        return build_header_pair(buf, s + 4, e - 1, HttpHeader::Age);
-                    }
-                }
                 b'c' => { /* Ac */
                     match buf[s + 4] {
                         b'p' => { /* Ac[ce]p */
-                            if buf[s+6] == b':' {
+                            if len_match(buf,s,e,6) {
                                 return build_header_pair(buf, s+7, e-1, HttpHeader::Accept);
                             }
                             match buf[s + 7] {
                                 b'P' => { /* Ac[ce]p[t-]P */
-                                    if buf[s+12] == b':' {
+                                    if len_match(buf,s,e,12) {
                                         return build_header_pair(buf, s+8, e-1, HttpHeader::AcceptPatch);
                                     }
                                 }
                                 b'R' => { /* Ac[ce]p[t-]R */
-                                    if buf[s+12] == b':' {
+                                    if len_match(buf,s,e,12) {
                                         return build_header_pair(buf, s+8, e-1, HttpHeader::AcceptRanges);
                                     }
                                 }
@@ -109,17 +102,59 @@ pub fn decode_header(buf: &[u8], s: usize, e: usize) -> Option<HttpHeaderPair> {
                                 b'A' => { /* Access-Control-A */
                                     match buf[s+22] {
                                         b'O' => { /* Access-Control-Allow-O */
-                                            if buf[s+27] == b':' && s+27 < e { // TODO: Add end checks to all ':' checks
+                                            if len_match(buf,s,e,27) {
                                                 return build_header_pair(buf, s+28, e-1, HttpHeader::AccessControlAllowOrigin);
                                             }
                                         }
                                         b'C' => {
-                                            if buf[s+32] == b':'
-                                        } 
+                                            if len_match(buf,s,e,32) {
+                                                return build_header_pair(buf, s+33, e-1, HttpHeader::AccessControlAllowCredentials);
+                                            }
+                                        }
+                                        b'M' => {
+                                            if len_match(buf,s,e,28) {
+                                                return build_header_pair(buf, s+29, e-1, HttpHeader::AccessControlAllowMethods);
+                                            }
+                                        }
+                                        b'H' => {
+                                            if len_match(buf,s,e,28) {
+                                                return build_header_pair(buf, s+29, e-1, HttpHeader::AccessControlAllowHeaders);
+                                            }
+                                        }
                                         _ => {}
                                     }
                                 }
+                                b'E' => {
+                                    if len_match(buf,s,e,29) {
+                                        return build_header_pair(buf, s+30, e-1, HttpHeader::AccessControlExposeHeaders);
+                                    }
+                                }
+                                b'M' => {
+                                    if len_match(buf,s,e,22) {
+                                        return build_header_pair(buf, s+23, e-1, HttpHeader::AccessControlMaxAge);
+                                    }
+                                }
                                 _ => {}
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                b'g' => { /* Ag */
+                    if len_match(buf, s, e, 3) {
+                        return build_header_pair(buf, s + 4, e - 1, HttpHeader::Age);
+                    }
+                }
+                b'l' => { /* Al */
+                    match buf[s+2] {
+                        b'l' => { /* All */
+                            if len_match(buf,s,e,5) {
+                                return build_header_pair(buf, s+6, e-1, HttpHeader::Allow);
+                            }
+                        }
+                        b't' => { /* Alt */
+                            if len_match(buf,s,e,7) {
+                                return build_header_pair(buf, s+8, e-1, HttpHeader::AltSvc);
                             }
                         }
                         _ => {}
@@ -130,22 +165,63 @@ pub fn decode_header(buf: &[u8], s: usize, e: usize) -> Option<HttpHeaderPair> {
         }
         b'C' => { /* C */
             match buf[s + 1] {
+                b'a' => { /* Ca */
+                    if len_match(buf,s,e,13) {
+                        return build_header_pair(buf, s+14, e-1, HttpHeader::CacheControl);
+                    }
+                }
                 b'o' => { /* Co */
                     match buf[s + 3] {
-                        b't' => { /* Co[n]t */
+                        b'n' => { /* Con */
+                            if len_match(buf,s,e,10) { // TODO: deal with open conns
+                                return build_header_pair(buf, s+11, e-1, HttpHeader::Connection);
+                            }
+                        }
+                        b't' => { /* Cont */
                             match buf[s + 8] {
-                                b'L' => { /* Co[n]t[ent-]L */
-                                    match buf[s + 9] {
-                                        b'e' => { /* Co[n]t[ent-]Le */
-                                            match buf[s + 14] {
-                                                b':' => { /* Co[n]t[ent-]Le[ngth]: */
-                                                    return build_header_pair(buf, s + 15, e - 1, HttpHeader::ContentLength);
-                                                }
-                                                _ => {
-                                                }
+                                b'D' => { /* Content-D */
+                                    if len_match(buf,s,e,19) {
+                                        return build_header_pair(buf, s+20, e-1, HttpHeader::ContentDisposition);
+                                    }
+                                }
+                                b'E' => { /* Content-E */
+                                    if len_match(buf,s,e,16) {
+                                        return build_header_pair(buf, s+17, e-1, HttpHeader::ContentEncoding);
+                                    }
+                                }
+                                b'L' => { /* Content-L */
+                                    match buf[s+ 9] {
+                                        b'a' => { /* Content-La */
+                                            if len_match(buf,s,e,16) {
+                                                return build_header_pair(buf, s+17, e-1, HttpHeader::ContentLanguage);
+                                            }
+                                        }
+                                        b'e' => { /* Content-Le */
+                                            if len_match(buf,s,e,14) {
+                                                return build_header_pair(buf, s + 15, e - 1, HttpHeader::ContentLength);
+                                            }
+                                        }
+                                        b'o' => { /* Content-Lo */
+                                            if len_match(buf,s,e,16) {
+                                                return build_header_pair(buf, s+17, e-1, HttpHeader::ContentLocation);
                                             }
                                         }
                                         _ => {}
+                                    }
+                                }
+                                b'R' => { /* Content-R */
+                                    if len_match(buf,s,e,13) {
+                                        return build_header_pair(buf, s+14, e-1, HttpHeader::ContentRange);
+                                    }
+                                }
+                                b'T' => { /* Content-T */
+                                    if len_match(buf,s,e,12) {
+                                        return build_header_pair(buf, s+13, e-1, HttpHeader::ContentType);
+                                    }
+                                }
+                                b'S' => { /* Content-S */
+                                    if len_match(buf,s,e,23) {
+                                        return build_header_pair(buf, s+24, e-1, HttpHeader::ContentSecurityPolicy);
                                     }
                                 }
                                 _ => {
@@ -160,8 +236,62 @@ pub fn decode_header(buf: &[u8], s: usize, e: usize) -> Option<HttpHeaderPair> {
                 }
             }
         }
-        _ => {
+        b'D' => { /* D */
+            match buf[s+1] {
+                b'a' => { /* Da */
+                    if len_match(buf,s,e,4) {
+                        return build_header_pair(buf, s+5, e-1, HttpHeader::Date);
+                    }
+                }
+                b'e' => { /* De */
+                    if len_match(buf,s,e,10) {
+                        return build_header_pair(buf, s+11, e-1, HttpHeader::DeltaBase);
+                    }
+                }
+                _ => {}
+            }
         }
+        b'E' => { /* E */
+            match buf[s+1] {
+                b'T' => { /* ET */
+                    if len_match(buf,s,e,4) {
+                        return build_header_pair(buf, s+5, e-1, HttpHeader::ETag);
+                    }
+                }
+                b'x' => { /* Ex */
+                    if len_match(buf,s,e,7) {
+                        return build_header_pair(buf, s+8, e-1, HttpHeader::Expires);
+                    }
+                }
+                _ => {}
+            }
+        }
+        b'I' => { /* I */
+            if len_match(buf,s,e,2) {
+                return build_header_pair(buf, s+3, e-1, HttpHeader::IM);
+            }
+        }
+        b'L' => { /* L */
+            match buf[s+1] {
+                b'a' => { /* La */
+                    if len_match(buf,s,e,13) {
+                        return build_header_pair(buf, s+14, e-1, HttpHeader::LastModified);
+                    }
+                }
+                b'i' => { /* Li */
+                    if len_match(buf,s,e,4) {
+                        return build_header_pair(buf, s+5, e-1, HttpHeader::Link);
+                    }
+                }
+                b'o' => {
+                    if len_match(buf,s,e,8) {
+                        return build_header_pair(buf, s+9, e-1, HttpHeader::Location);
+                    }
+                }
+                _ => {}
+            }
+        }
+        _ => {}
     }
 
     return None;
